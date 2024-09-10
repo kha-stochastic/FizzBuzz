a42987ce96f0c1354aca2c12fa8c9713444a6b50
lejas <kha.le@mail.utoronto.ca>
2024-09-09 22:37:35 -0400

    commit

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,12 +5,12 @@ class FizzBuzz {
 
     public static void main(String[] args) {
         int i = 1;
-        while (i<100) {
+        while (i < 100) {
 
             i = doFizzBuzz(i);
 
         }
-        }
+    }
 
     private static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
@@ -34,7 +34,8 @@ class FizzBuzz {
 
             System.out.println(i);
         }
-        {i++;
+        {
+            i++;
         }
         return i;
     }
